7e45de6a9f266b20344dc467a3a0834f91c18f5f
Ant76.ch8 <antoine.ch76@gmail.com>
2022-02-24 15:55:29 +0100

    modification fond accueil et fond poemes

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -2,7 +2,7 @@ const nav = document.querySelector(".menuAnime");
 
 window.addEventListener("scroll", () => {
 
-    if (window.scrollY > 300){
+    if (window.scrollY > 400){
         nav.style.top = 0;
     } else {
         nav.style.top = "-100px";
