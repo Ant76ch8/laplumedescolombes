952e66d4d6be573c1c496ec3def294c0b2e4dd3b
Ant76.ch8 <antoine.ch76@gmail.com>
2022-03-25 22:28:14 +0100

    animation page accueil

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,4 +1,7 @@
 const nav = document.querySelector(".menuAnime");
+const presentationAccueil = document.querySelector(".presentation_anime");
+const adhesionAccueil = document.querySelector(".adhesion_anime");
+
 
 window.addEventListener("scroll", () => {
 
@@ -7,4 +10,12 @@ window.addEventListener("scroll", () => {
     } else {
         nav.style.top = "-100px";
     }
+
+    if (window.scrollY > 1200){
+        presentationAccueil.className = "col-xl-8 d-flex flex-column align-items-center visible presentation_anime";
+    }
+    if (window.scrollY > 1500){
+        adhesionAccueil.className = "col-xl-8 d-flex flex-column align-items-center visible adhesion_anime";
+    }
+    
 });
